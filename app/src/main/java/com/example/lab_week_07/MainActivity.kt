26b0bce5b08732057d3c8507be735fc858091ff0
Xins22 <162821328+Xins22@users.noreply.github.com>
2025-10-12 19:18:52 +0700

    Commit No. 1 – Create MapsActivity and location permission request

--- app/src/main/java/com/example/lab_week_07/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_07/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.lab_week_07
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
